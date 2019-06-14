811cf982785f71b6034b08717b478a80649f9d59
unknown <dannyfsociety@gmail.com>
2019-06-14 11:53:48 -0400

    Initial commit Data access layer implemented

--- ImageGallery/Controllers/HomeController.cs
+++ ImageGallery/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace ImageGallery.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "your application description page.";
 
             return View();
         }
